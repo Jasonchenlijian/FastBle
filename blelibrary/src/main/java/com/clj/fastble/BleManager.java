6e985de0e64242576db64c67f48a4c48be447de3
Lijian <1033526540@qq.com>
2016-08-29 15:59:27 +0800

    add disableBuletooth

--- blelibrary/src/main/java/com/clj/fastble/BleManager.java
+++ blelibrary/src/main/java/com/clj/fastble/BleManager.java
@@ -141,6 +141,15 @@ public class BleManager {
     }
 
     /**
+     * 关闭蓝牙
+     */
+    public void disableBluetooth() {
+        if (bleBluetooth != null) {
+            bleBluetooth.disableBluetooth();
+        }
+    }
+
+    /**
      * 扫描到周围第一个符合名称的设备即连接，并持续监听与这个设备的连接状态
      */
     private boolean scanAndConnect(String deviceName, long time_out, final BleManagerConnectCallback connectCallback) {
